47ea30ae4838ea6779eb85c64fbb15cc2180b703
Arred <arredondo_luis@live.com.mx>
2019-08-29 16:31:10 -0600

    Forof accediendo al valor de variable de string

---
+++ Libro2/forof2.js
@@ -0,0 +1,5 @@
+var string = "is	it	me	you're	looking	for?";
+for (var _i = 0, string_1 = string; _i < string_1.length; _i++) {
+    var char = string_1[_i];
+    console.log(char); //	is	it	me	you're	looking	for? 
+}
---
+++ Libro2/forof2.ts
@@ -0,0 +1,4 @@
+let	string	=	"is	it	me	you're	looking	for?";
+for	(let char of string)	{				
+    console.log(char);	//	is	it	me	you're	looking	for? 
+}
