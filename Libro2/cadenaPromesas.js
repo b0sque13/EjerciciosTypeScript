8d349148753f3b6af0da4713f46ddd86527ad95e
Arred <arredondo_luis@live.com.mx>
2019-09-01 11:27:39 -0600

    Promesas y cadenas de promesas

---
+++ Libro2/cadenaPromesas.js
@@ -0,0 +1,15 @@
+Promise.reject(new Error('something	bad	happened'))
+    .then(function (res) {
+    console.log(res); //	not	called								
+    return 456;
+})
+    .then(function (res) {
+    console.log(res); //	not	called								
+    return Promise.resolve(123);
+})
+    .then(function (res) {
+    console.log(res); //	not	called							
+    return Promise.resolve(123);
+})["catch"](function (err) {
+    console.log(err.message); //	something	bad	happened												
+});
---
+++ Libro2/cadenaPromesas.ts
@@ -0,0 +1,16 @@
+Promise.reject(new Error('something	bad	happened'))
+    .then((res) => {
+        console.log(res);	//	not	called								
+        return 456;
+    })
+    .then((res) => {
+        console.log(res);	//	not	called								
+        return Promise.resolve(123);
+    })
+    .then((res) => {
+        console.log(res);	//	not	called							
+        return Promise.resolve(123);
+    })
+    .catch((err) => {
+        console.log(err.message);	//	something	bad	happened												
+    });
